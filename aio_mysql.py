0f2e9ddb529173df509db0ac757f243f9d8e68e1
china-shang <850747813@qq.com>
2018-10-26 16:24:08 +0800

    fix bug

--- aio_mysql.py
+++ aio_mysql.py
@@ -129,6 +129,8 @@ class QueuePool(object):
 
                 self._will_user_fetched.difference_update(s2)
                 self._user_fetched.difference_update(s2)
+                s1.clear()
+                s2.clear()
                 logger.info(f"{self._fetched}{self._user_fetched}{self._will_fetched}{self._will_user_fetched}{s1}{s2}")
                 del s1, s2
 
